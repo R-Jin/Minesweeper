08a61f9c880e994176e3448b9e00fe9095eb55ed
R-Jin <ryanjin007@gmail.com>
2024-03-07 21:24:56 +0100

    User input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use macroquad::prelude::*;
 use macroquad::window::Conf;
 
-
 fn window_conf() -> Conf {
     Conf {
         window_title: "Minesweeper".to_owned(),
@@ -16,6 +15,16 @@ async fn main() {
     // changing screen size
     loop {
         clear_background(WHITE);
+        // Input
+        let pos = mouse_position();
+        if is_mouse_button_pressed(MouseButton::Left) {
+            println!("x: {}, y: {}", pos.0, pos.1)
+        }
+
+        // Update
+
+        // Draw
+
         next_frame().await
     }
 }
