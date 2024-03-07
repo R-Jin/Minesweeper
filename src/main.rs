453e4f0781769b351c26a2f39bbb66eea4dd5505
R-Jin <ryanjin007@gmail.com>
2024-03-07 21:15:54 +0100

    Create window using macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+use macroquad::window::Conf;
+
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Minesweeper".to_owned(),
+        window_width: 500,
+        window_height: 500,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    // changing screen size
+    loop {
+        clear_background(WHITE);
+        next_frame().await
+    }
 }
