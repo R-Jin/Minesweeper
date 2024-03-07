7827795244d98d4908a5fc25587988de9c0b6d18
R-Jin <ryanjin007@gmail.com>
2024-03-07 23:35:51 +0100

    Create struct for board

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,36 @@ fn window_conf() -> Conf {
     }
 }
 
+#[derive(Clone, Debug)]
+enum CellState {
+    Mine,
+    Number(i32),
+    Empty,
+}
+
+struct Board {
+    x_cells: usize,
+    y_cells: usize,
+    state: Vec<Vec<CellState>>,
+}
+
+impl Board {
+    pub fn new(x_cells: usize, y_cells: usize) -> Self {
+        let row = vec![CellState::Empty; x_cells];
+        Self {
+            x_cells,
+            y_cells,
+            state: vec![row; y_cells],
+        }
+    }
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
     // changing screen size
+
+    // Initialize a board of size 16x16
+
     loop {
         clear_background(WHITE);
         // Input
