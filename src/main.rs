1182e0b421ced5cf73f20cea0faf6ced070c8f49
R-Jin <ryanjin007@gmail.com>
2024-03-26 22:57:37 +0100

    Draw board is now possible

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,45 @@ enum CellState {
     Mine,
     Number(i32),
     Empty,
+    Hidden,
 }
 
 struct Board {
     x_cells: usize,
     y_cells: usize,
+    gap: f32,
+    padding: f32,
     state: Vec<Vec<CellState>>,
 }
 
 impl Board {
-    pub fn new(x_cells: usize, y_cells: usize) -> Self {
-        let row = vec![CellState::Empty; x_cells];
+    pub fn new(x_cells: usize, y_cells: usize, gap: f32, padding: f32) -> Self {
+        let row = vec![CellState::Hidden; x_cells];
         Self {
             x_cells,
             y_cells,
+            gap,
+            padding,
             state: vec![row; y_cells],
+        }
+    }
+
+    pub fn draw_board(&self) {
+        let tile_width: f32 =
+            (screen_width() - self.padding - self.gap * self.x_cells as f32) / self.x_cells as f32;
+
+        for row in 0..self.y_cells {
+            for col in 0..self.x_cells {
+                let x = self.padding as f32 + col as f32 * (self.gap + tile_width);
+                let y = self.padding as f32 + row as f32 * (self.gap + tile_width);
+                let c = match self.state[row][col] {
+                    CellState::Mine => BLACK,
+                    CellState::Empty => GREEN,
+                    CellState::Number(_) => PINK,
+                    CellState::Hidden => GRAY,
+                };
+                draw_rectangle(x, y, tile_width, tile_width, c);
+            }
         }
     }
 }
@@ -37,8 +61,7 @@ impl Board {
 #[macroquad::main(window_conf)]
 async fn main() {
     // changing screen size
-
-    // Initialize a board of size 16x16
+    let board = Board::new(16, 16, 1.0, 2.0);
 
     loop {
         clear_background(WHITE);
@@ -51,6 +74,7 @@ async fn main() {
         // Update
 
         // Draw
+        board.draw_board();
 
         next_frame().await
     }
